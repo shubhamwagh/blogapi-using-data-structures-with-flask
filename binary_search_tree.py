044a7932985c82496cba5fae46e8532f0801ec87
Shubhan Wagh <shubham.wagh@q-bot.co>
2021-11-15 18:02:51 +0000

    Added new data structures and completed server functions

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -9,23 +9,49 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def _insert_recursive(self, value, node):
-        if value < node.data:
+    def _insert_recursive(self, data, node):
+        if data["id"] < node.data["id"]:
             if node.left is None:
-                node.left = Node(value)
+                node.left = Node(data)
             else:
-                self._insert_recursive(value, node.left)
-        elif value > node.data:
+                self._insert_recursive(data, node.left)
+        elif data["id"] > node.data["id"]:
             if node.right is None:
-                node.right = Node(value)
+                node.right = Node(data)
             else:
-                self._insert_recursive(value, node.right)
+                self._insert_recursive(data, node.right)
 
         else:
             return
 
-    def insert(self, value):
+    def insert(self, data):
         if self.root is None:
-            self.root = Node(value)
+            self.root = Node(data)
         else:
-            self._insert_recursive(value, self.root)
+            self._insert_recursive(data, self.root)
+
+    def _search_recursive(self, blog_post_id, node):
+        # if node.left is None and node.right is None:
+        #     return False
+
+        if blog_post_id == node.data["id"]:
+            return node.data
+
+        if blog_post_id < node.data["id"] and node.left is not None:
+            if blog_post_id == node.left.data["id"]:
+                return node.left.data
+            return self._search_recursive(blog_post_id, node.left)
+
+        if blog_post_id > node.data["id"] and node.right is not None:
+            if blog_post_id == node.right.data["id"]:
+                return node.right.data
+            return self._search_recursive(blog_post_id, node.right)
+
+        return False
+
+    def search(self, blog_post_id):
+        blog_post_id = int(blog_post_id)
+        if self.root is None:
+            return False
+
+        return self._search_recursive(blog_post_id, self.root)
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import random
 from sqlite3 import Connection as SQLite3Connection
 from datetime import datetime
 from sqlalchemy import event
@@ -7,6 +8,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 from linked_list import LinkedList
 from hash_table import HashTable
+from binary_search_tree import BinarySearchTree
+from custom_queue import Queue
+from stack import Stack
 
 # app
 app = Flask(__name__)
@@ -161,19 +165,74 @@ def create_blog_post(user_id):
 
 
 @app.route("/blog_post/<blog_post_id>", methods=["GET"])
-def get_all_blog_posts(blog_post_id):
-    blog_posts = BlogPost.query.all()
-    
-
-
-@app.route("/blog_post/<blog_post_id>", methods=["GET"])
 def get_one_blog_post(blog_post_id):
-    pass
+    blog_posts = BlogPost.query.all()
+    random.shuffle(blog_posts)
+
+    bst = BinarySearchTree()
+
+    for post in blog_posts:
+        bst.insert({
+            "id": post.id,
+            "title": post.title,
+            "body": post.body,
+            "user_id": post.user_id,
+        })
+
+    post = bst.search(blog_post_id)
+
+    if not post:
+        return jsonify({"message": "post not found"})
+
+    return jsonify(post)
 
 
-@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
-def delete_blog_post(blog_post_id):
-    pass
+@app.route("/blog_post/numeric_body", methods=["GET"])
+def get_numeric_post_bodies():
+    blog_posts = BlogPost.query.all()
+
+    q = Queue()
+
+    for post in blog_posts:
+        q.enqueue(post)
+
+    return_list = []
+
+    for _ in range(len(blog_posts)):
+        post = q.dequeue()
+        numeric_body = 0
+        for char in post.data.body:
+            numeric_body += ord(char)
+
+        post.data.body = numeric_body
+
+        return_list.append(
+            {
+                "id": post.data.id,
+                "title": post.data.title,
+                "body": post.data.body,
+                "user_id": post.data.user_id,
+            }
+        )
+
+    return jsonify(return_list)
+
+
+@app.route("/blog_post/delete_last_10", methods=["DELETE"])
+def delete_last_10():
+    blog_posts = BlogPost.query.all()
+
+    s = Stack()
+
+    for post in blog_posts:
+        s.push(post)
+
+    for _ in range(10):
+        post_to_delete = s.pop()
+        db.session.delete(post_to_delete.data)
+        db.session.commit()
+
+    return jsonify({"message": "success"})
 
 
 if __name__ == "__main__":
